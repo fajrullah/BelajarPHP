2d6228dce3945e59943993c4b5f41123d8462051
Fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-04-13 17:14:24 +0700

    Insert Login

--- database.sql
+++ database.sql
@@ -21,3 +21,18 @@ CREATE TABLE `detail_product` (
   PRIMARY KEY  (`id_detail_product`)
 );
 
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`id`, `nama`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin'),
+(2, 'staff', 'staff', 'staff', 'staff')
+
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
