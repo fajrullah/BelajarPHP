2cf95ab84130162ae84b766380dad29f20a89f5f
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-05-12 12:13:25 +0700

    XLS

---
+++ exportxls.php
@@ -0,0 +1,66 @@
+<?php 
+
+header("Content-type: application/");
+
+header("Content-Disposition: attachment; filename=Laporan Semester 1.xls");
+
+header("Pragma: no-cache");
+
+header("Expires: 0");
+
+?>
+
+<table id="orderlist1" border="1" width="100%">
+
+<thead>
+
+<tr>
+	<th>satu</th>
+	<th>satu</th>
+	<th>satu</th>
+	<th>satu</th>
+	<th>satu</th>
+ </tr>
+</thead>
+<tbody>
+	<tr> 		
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+
+	</tr>
+		<tr> 		
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+
+	</tr>
+		<tr> 		
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+	</tr>
+		<tr> 		
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+	</tr>
+		<tr> 		
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			<td>dua</td>
+			
+	</tr>
+
+</tbody>
+</table>
